b207762e53909349e3327612638819f73b5bc86a
SameerVerma066 <sameerverma1258@gmail.com>
2025-02-24 11:22:25 +0530

    classname

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,11 +69,11 @@ const Hero = () => {
         {/* Buttons */}
         <div className="mt-8 flex justify-start space-x-4">
         <Link to="/our-services" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-gold rounded-full shadow-md group">
-         <span classname="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-gold group-hover:translate-x-0 ease">
-         <svg classname="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+         <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-gold group-hover:translate-x-0 ease">
+         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
          </span>
-         <span classname="absolute flex items-center justify-center w-full h-full font-nunito text-white transition-all duration-300 transform group-hover:translate-x-full ease">Our Services</span>
-         <span classname="relative invisible">Our Services</span>
+         <span class="absolute flex items-center justify-center w-full h-full font-nunito text-white transition-all duration-300 transform group-hover:translate-x-full ease">Our Services</span>
+         <span class="relative invisible">Our Services</span>
           </Link>
           <ScrollLink 
          to="footer" 
